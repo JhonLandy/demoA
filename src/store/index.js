a9ab5f1578e3f2e3310019c15f81e14ea0ecd803
YuanChengLang <709364178@qq.com>
2020-03-29 20:35:13 +0800

    '页面'

---
+++ src/store/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+    state: {
+        userInfo: {
+            name: 'ycl',
+            sex: 'man'
+        }
+    },
+    mutations: {
+        changeUser(state, data) {
+            state.userInfo = data;
+        }
+    }
+});
+
+export default store;
