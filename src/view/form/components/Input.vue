a58066f755003be2c910f7abe14a82d873e734a0
wb.yuanchenglang <wb.yuanchenglang@mesg.corp.netease.com>
2020-05-15 19:32:38 +0800

    封装表单

---
+++ src/view/form/components/Input.vue
@@ -0,0 +1,22 @@
+<template>
+    <input class="y-input" type="text" @input="inputChange" v-model="inputValue"/>
+</template>
+<script>
+    export default {
+        name: 'y-input',
+        data() {
+            return {
+                inputValue: ""
+            }
+        },
+        methods: {
+            inputChange(e) {
+                if(this._events.input) {
+                    this.$emit("input", this.inputValue)
+                } else {
+                    this.inputValue = "";
+                }
+            }
+        }
+    }
+</script>>
---
+++ src/view/form/index.vue
@@ -0,0 +1,107 @@
+<template>
+    <s-form :search-lists="searchItem" :option-link-map="optionLinkMap" @search="getInstanceList"></s-form>
+</template>
+
+<script>
+    import SForm from "@/view/form/components/Sform"
+    // import Input from "@/views/form/components/Input"
+    // import Select from "@/views/form/components/Select"
+    // import SelectOption from "@/views/form/components/SelectOption"
+    
+    // Vue.prototype.$bus = new Vue();
+
+    // Vue.component('y-form', Form);
+    // Vue.component('y-input', Input);
+    // Vue.component('y-select', Select);
+    // Vue.component('y-option', SelectOption);
+    export default {
+        name: 'formComponent',
+        data() {
+            return {
+                searchItem: [
+                    {
+                        searchParam: 'project',
+                        type: 'select',
+                        label: '项目',
+                        defaultShow: true,
+                        multiple: true,
+                        filterable: true,
+                        loading: true,
+                        remote: true,
+                        options: 'project',
+                        permission: () => {
+                            return this.isAdmin
+                        }
+                    },
+                    {
+                        searchParam: 'dc_code',
+                        type: 'selectGroup',
+                        label: '区域',
+                        multiple: true,
+                        filterable: true,
+                        loading: true,
+                        options: 'area'
+                    },
+                    {
+                        type: 'input',
+                        label: '实例ID',
+                        searchParam: 'id'
+                    },
+                    {
+                        searchParam: 'use_case',
+                        type: 'radioButton',
+                        label: '用途',
+                        options: [
+                            {
+                                value: '1',
+                                label: '正式'
+                            },
+                            {
+                                value: '2',
+                                label: '测试'
+                            }
+                        ]
+                    },
+                    {
+                        searchParam: 'status',
+                        type: 'radioButton',
+                        label: '状态',
+                        options: [
+                            {
+                                value: '3',
+                                label: 'RUNNING'
+                            },
+                            {
+                                value: '4',
+                                label: 'PENDING'
+                            }
+                        ]
+                    }
+                ],
+                optionLinkMap: {
+                    area: [],
+                    project: []
+                }
+            }
+        },
+        components: {
+            SForm
+        },
+        methods: {
+            valid(isPass) {
+                isPass && this.$message({
+                    message: '保存成功',
+                    type: 'success'
+                });
+            },
+            getInstanceList() {
+
+            }
+        }
+    }
+</script>
+<style scoped>
+    .el-form {
+        padding: 10px
+    }
+</style>
