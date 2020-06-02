fc6ed66d61f782a934d21f4e2d3081af87c954d1
YuanChengLang <709364178@qq.com>
2020-06-02 20:48:46 +0800

    'sa'

--- src/view/table/table.vue
+++ src/view/table/table.vue
@@ -100,7 +100,6 @@
          watch: {
             page: function (val) {
                 this.getData(val, this.limit)
-                console.log('page')
             },
              limit: function (val) {
                  this.getData(this.page, val)
