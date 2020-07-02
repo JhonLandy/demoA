1f2981d7438bf1177aa2d1193bf7f132b02d945e
YuanChengLang <709364178@qq.com>
2020-07-02 22:47:25 +0800

    更新react-redux源码

--- src/React-redux/index.js
+++ src/React-redux/index.js
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react';
+import React, {useContext, useReducer} from 'react';
 
 const Connect = React.createContext()
 
@@ -11,25 +11,71 @@ export function Provider({children, store}) {
     )
 }
 
+export function bindActionCreator(creater, dispatch) {
+    return function(...args) {
+        return dispatch(creater(...args))
+    }
+}
+
+export function bindActionCreators(creater, dispatch) {
+    let obj = {}
+    for (const key in creater) {
+        obj[key] = bindActionCreator(creater[key], dispatch)
+    } 
+    return obj
+}
+
+
 export function connect(mapStateToProps, mapDispatchToProps) {
     const {store} = useContext(Connect)
-    const {getState} = store 
+    const {getState, dispatch} = store 
     const newState = mapStateToProps(getState())
 
     const newDispatch = mapDispatchToProps()
+    let dispatchProps
+    if (typeof mapDispatchToProps === 'function') {
+        dispatchProps = mapStateToProps(dispatch)
+    } else {
+        dispatchProps = bindActionCreators(mapDispatchToProps, dispatch)
+    }
+
     return function(cmp) {
+        return function (props) { 
         return (
-            <cmp {...newState} {...newDispatch}/>
+            <cmp {...newState} {...dispatchProps} {...props}/>
         )
     }
 }
 
-export function useSelector() {
+export function useSelector(selecter) {
+    const store = useStore()
 
+    const {getState, subscribe} = store
+    let [_, forroceUpdate] = useReducer(x => x + 1, 0)
+    
+    useLayoutEffect(() => {
+        
+        let unsubscribe = subscribe(() => {
+            forroceUpdate()
+        })
+       
+        return () => {
+            unsubscribe()
+        }
+    }, [store])
+
+
+
+    return selecter(getState())
 }
 
 export function useDispatch() {
+    const store = useStore()
+    return store.dispatch()
+}
 
+export function useStore() {
+    const {store} = useContext(Connect)
 
-    
+    return store;
 }
